4a1a26256552574ce9f28f74b4f8f3b9a4025306
GurungRegina <gina524286@gmail.com>
2021-04-06 16:50:08 +0100

    Create decorators.py

---
+++ Functions_Introspection/decorators.py
@@ -0,0 +1,9 @@
+"""
+Decorators in general:
+* takes a function as an argument
+* returns a closure
+* the closure usually accepts any combination of parameters
+* runs some code in the inner function (closure)
+* the closure function calls the original function using the arguments passed to the closure.
+
+"""
