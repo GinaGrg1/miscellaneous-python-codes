140242991b22a11c8cb9b9c049816e94e6258867
GurungRegina <gina524286@gmail.com>
2021-04-13 09:22:49 +0100

    Update decorators.py

--- Functions_Introspection/decorators.py
+++ Functions_Introspection/decorators.py
@@ -6,4 +6,26 @@ Decorators in general:
 * runs some code in the inner function (closure)
 * the closure function calls the original function using the arguments passed to the closure.
 
+* wraps can be used to fix the metadata of our inner function in the decorator.
 """
+
+from functools import wraps
+
+def counter(fn):
+  count = 0
+  
+  @wraps
+  def inner(*args, **kwargs):
+    nonlocal count
+    count += 1
+    print("{0} has been called {1} times".format(fn.__name__, count))
+    #counters[fn.__name__] = count
+    return fn(*args, **kwargs)  # fn is a closure.
+  return inner
+
+def add(a, b):
+  return a + b
+
+@counter
+def mult(a, b):
+  return a * b
